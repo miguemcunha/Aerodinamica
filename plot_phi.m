c046a57461cc18a307fdd6604a1b698cbacc4fd3
miguemcunha <miguel.cunha3519@gmail.com>
2021-11-15 16:12:54 +0000

    almost

--- plot_phi.m
+++ plot_phi.m
@@ -2,17 +2,17 @@ function plot_phi(phi, i)
     figure
     if i == 0
         [x,y] = meshgrid(-5:0.1:5,-5:0.1:5);
-        contourf(x,y,phi,50,'--');
+        [~,h] = contour(x,y,phi,60,'--');
     else
         [r, theta] = meshgrid(-5:0.1:5,-2*pi:0.1:2*pi);
-        contourf(r,theta,phi,50,'--');
+        [~,h] = contour(r,theta,phi,50,'-');
     end
     hold on;
     title('Sobreposição de escoamentos: Linhas equipotenciais');
+    h.LineWidth = 2;
+    set(h, 'Color', 'b');
     hold off;
     hold on;
-    t = colorbar;
-    t.Label.String = 'Magnitude';
     hold on;
     axis tight;
     axis equal;
